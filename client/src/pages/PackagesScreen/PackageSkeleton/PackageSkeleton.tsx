c89bb40f3af94f5538b0396d703de8c508c85cb5
deniskod <deniskod20025@gmail.com>
2025-03-29 15:30:38 +0300

    feat: added skeleton to packages

---
+++ client/src/pages/PackagesScreen/PackageSkeleton/PackageSkeleton.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { Skeleton } from 'antd';
+import styles from './package-skeleton.module.scss';
+
+const PackageSkeleton = () => {
+    return (
+        <>
+            {Array.from({ length: 3 }).map((_, index) => (
+                <div className={styles.skeletonCard} key={index}>
+                    <div className={styles.skeletonMatches}>
+                        {Array.from({ length: 2 }).map((_, matchIndex) => (
+                            <div className={styles.skeletonMatchItem} key={matchIndex}>
+                                <Skeleton.Avatar shape="square" size={64} active />
+                                <div>
+                                    <Skeleton.Input style={{ width: 150 }} active />
+                                    <Skeleton.Input style={{ width: 100, marginTop: 4 }} active />
+                                </div>
+                                {matchIndex !== 1 && (
+                                    <ArrowRightOutlined className={styles.skeletonArrowIcon} />
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                    <div className={styles.skeletonFooter}>
+                        <Skeleton.Button style={{ width: 120 }} active />
+                        <Skeleton.Button style={{ width: 80, marginLeft: 'auto' }} active />
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default PackageSkeleton;
