3b700bf7601a2a879aac27e9559210fd4406d549
deniskod <deniskod20025@gmail.com>
2025-03-30 23:35:24 +0300

    fix

--- server/src/controllers/geo.controller.ts
+++ server/src/controllers/geo.controller.ts
@@ -1,7 +1,7 @@
-import {Request, Response} from 'express';
-import {AmadeusService} from '../services/amadeus.service';
-import {CityLocationSchema, CitySearchParams, CitySearchParamsSchema} from '../models/geo.model';
-import {soccerService} from '../services/soccer.service';
+import { Request, Response } from 'express';
+import { AmadeusService } from '../services/amadeus.service';
+import { CityLocationSchema, CitySearchParams, CitySearchParamsSchema } from '../models/geo.model';
+import { soccerService } from '../services/soccer.service';
 
 export const geoController = {
     getCountries: async (_req: Request, res: Response) => {
@@ -9,35 +9,30 @@ export const geoController = {
             const countries = await soccerService.getCountries();
             res.status(200).json(countries);
         } catch (e) {
-            res.status(500).json({message: 'Error fetching countries', error: e});
+            res.status(500).json({ message: 'Error fetching countries', error: e });
         }
     },
     getCities: async (req: Request<any, any, CitySearchParams>, res: Response) => {
         const { data: parsedParams, error } = CitySearchParamsSchema.safeParse(req.query);
         if (error) {
-          res.status(400).json(error.errors);
-          return;
-        }
-      
-        try {
-          const cities = await AmadeusService.getCities(parsedParams);
-      
-          if (!Array.isArray(cities)) {
-            res.status(200).json([]);
+            res.status(400).json(error.errors);
             return;
-          }
-      
-          const parsedCities = CityLocationSchema.array().parse(cities);
-      
-          if (parsedParams.withIataCode) {
-            const citiesWithIATA = parsedCities.filter((city) => !!city.iataCode);
-            res.status(200).send(citiesWithIATA);
-          } else {
-            res.status(200).send(parsedCities);
-          }
-        } catch (err) {
-          console.error('Failed to fetch or parse cities:', err);
-          res.status(500).json({ message: 'Failed to fetch cities', error: err });
         }
-      },
+
+        try {
+            const cities = (await AmadeusService.getCities(parsedParams)) ?? [];
+
+            const parsedCities = CityLocationSchema.array().parse(cities);
+
+            if (parsedParams.withIataCode) {
+                const citiesWithIATA = parsedCities.filter((city) => !!city.iataCode);
+                res.status(200).send(citiesWithIATA);
+            } else {
+                res.status(200).send(parsedCities);
+            }
+        } catch (err) {
+            console.error('Failed to fetch or parse cities:', err);
+            res.status(500).json({ message: 'Failed to fetch cities', error: err });
+        }
+    },
 };
