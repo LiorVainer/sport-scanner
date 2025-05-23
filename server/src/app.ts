37f49a00e6c02f16d2b0fdb0649ee95a7f9ead65
Lior Vainer <68500353+LiorVainer@users.noreply.github.com>
2025-05-23 11:33:37 +0300

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,26 +1,25 @@
 import http from 'http';
 import https from 'https';
 import { Server } from 'socket.io';
-import { initServer, app } from './server';
-import path from 'path';
-import express from 'express';
-import fs from 'fs';
-import { initializeSocket } from './socket';
+import { app, initServer } from './server';
 import { ENV } from './env/env.config';
+import { initializeSocket } from './socket';
 
 const isProduction = ENV.NODE_ENV === 'production';
 
 let server: http.Server | https.Server;
 
-if (isProduction) {
-    const options = {
-        key: fs.readFileSync('./client-key.pem'),
-        cert: fs.readFileSync('./client-cert.pem'),
-    };
-    server = https.createServer(options, app);
-} else {
-    server = http.createServer(app);
-}
+// if (isProduction) {
+//     const options = {
+//         key: fs.readFileSync('./client-key.pem'),
+//         cert: fs.readFileSync('./client-cert.pem'),
+//     };
+//     server = https.createServer(options, app);
+// } else {
+//     server = http.createServer(app);
+// }
+
+server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
@@ -31,13 +30,13 @@ const io = new Server(server, {
 
 initServer()
     .then(() => {
-        app.use(express.static(path.join(__dirname, '../../web-final-project-client/dist')));
-
+        // app.use(express.static(path.join(__dirname, '../../web-final-project-client/dist')));
+        //
         initializeSocket(io);
-
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../../web-final-project-client/dist/index.html'));
-        });
+        //
+        // app.get('*', (req, res) => {
+        //     res.sendFile(path.join(__dirname, '../../web-final-project-client/dist/index.html'));
+        // });
 
         server.listen(ENV.PORT, () => {
             console.log(`✅ ${isProduction ? 'HTTPS' : 'HTTP'} Server listening on port ${ENV.PORT}`);
