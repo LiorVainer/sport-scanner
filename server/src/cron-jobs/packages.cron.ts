6f3ff07e5ac8dc7a4ecff0c79cfd78c4242000e0
Lior Vainer <liorvainer@gmail.com>
2025-09-06 21:04:38 +0300

    fix: Update cron job schedule for user package generation and add Vercel configuration file

--- server/src/cron-jobs/packages.cron.ts
+++ server/src/cron-jobs/packages.cron.ts
@@ -21,7 +21,7 @@ const MAX_PACKAGES_PER_USER_WITH_OFFSET =
     ENV.USER_SUGGESTED_PACKAGES_GENERATION_MAX_PACKAGES_PER_USER +
     ENV.USER_SUGGESTED_PACKAGES_GENERATION_MAX_PACKAGES_OFFSET;
 
-const schedule = ENV.NODE_ENV === 'development' ? CronTime.everyHourAt([46, 48, 50]) : CronTime.everyHourAt([2, 4, 6]);
+const schedule = ENV.NODE_ENV === 'development' ? CronTime.everyHour() : CronTime.everyFriday();
 
 /**
  * Cron Job that runs once a day at midnight (00:00).
@@ -129,7 +129,7 @@ cron.schedule(schedule, async () => {
                         timer.stop(`${ProcessTypes.USER_SUGGESTED_PACKAGES_GENERATION}_${user._id}`);
                         return;
                     }
-                    
+
                     const savedPackages = await PackageRepository.insertMany(generatedPackages);
 
                     await UserRepository.updateOne(
